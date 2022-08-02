a0faeb7ed90b7ac4c47220513d1e04e363f839ee
hrksum <hiroki.soma@persol.co.jp>
2022-08-02 01:30:32 +0000

    refactor: Moved themeprovider from header to app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,21 @@
 import type { AppProps } from 'next/app'
+import * as Primer from '@primer/react'
+
+export type ReactFCWrapper<T> = T extends React.FC<infer U>
+  ? React.FC<U & { children?: React.ReactNode }>
+  : T
+
+type ThemeProviderProps = Primer.ThemeProviderProps & {
+  children?: React.ReactNode
+}
+export const ThemeProvider = Primer.ThemeProvider as React.FC<ThemeProviderProps>
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
